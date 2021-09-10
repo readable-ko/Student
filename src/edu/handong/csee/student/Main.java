cbac27727b0604ec0aa71917bae2190490fd1d09
readable-ko <21700026@handong.edu>
2021-09-10 11:05:22 +0900

    updated index to 10

--- src/edu/handong/csee/student/Main.java
+++ src/edu/handong/csee/student/Main.java
@@ -11,17 +11,24 @@ public class Main {
 		List<Student> al = new ArrayList<Student>();
 		System.out.println("Student List (ordered by name)");
 		
-		Student s1 = new Student(101,"Kim",23);
+		Student s1 = new Student(101,"Daniel",23);
 		al.add(s1);
 		Student s2 = new Student(102,"Choi",21);
 		al.add(s2);
 		al.add(new Student(103,"Park",25));
+		al.add(new Student(104,"Ko",24));
+		al.add(new Student(105,"Lee",22));
+		al.add(new Student(106,"Moon",23));
+		al.add(new Student(107,"Abram",25));
+		al.add(new Student(108,"Hong",35));
+		al.add(new Student(109,"Seo",22));
+		al.add(new Student(110,"Jang",24));
 		
 		Collections.sort(al);
 		for(Student s : al) {
 			System.out.println(s.toString());
 		}
-		System.out.println("Student List (reverse ordered by name)");
+		System.out.println("\nStudent List (reverse ordered by name)");
 		
 		Collections.sort(al, Collections.reverseOrder());
 		
